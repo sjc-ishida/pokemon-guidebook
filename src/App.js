51b1b11c68aaf93cba16f32259d95507484eb3ab
ryuji-i <fantaneo.orange@gmail.com>
2024-05-04 09:24:29 +0900

    done chapter no.10

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import "./App.css";
+import { getAllPokemon } from "./utils/pokemon";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+    };
+    fetchPokemonData();
+  }, []);
 
   return <div className="App"></div>;
 }
---
+++ src/utils/pokemon.js
@@ -0,0 +1,8 @@
+export const getAllPokemon = (url) => {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch((error) => reject(error));
+  });
+};
