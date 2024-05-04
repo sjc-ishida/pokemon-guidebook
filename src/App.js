7270ca23655d72cb9a7435ab7baa53f0f255aa84
ryuji-i <fantaneo.orange@gmail.com>
2024-05-04 11:42:48 +0900

    done chapter no.18

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { getAllPokemon, getPokemon } from "./utils/pokemon";
 import Card from "./compoments/Card/Card";
+import Navbar from "./compoments/Navbar/Navbar";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
@@ -31,17 +32,20 @@ function App() {
   console.log(pokemonData);
 
   return (
-    <div className="App">
-      {loading ? (
-        <h1>ロード中・・・</h1>
-      ) : (
-        <div className="pokemonCardContainer">
-          {pokemonData.map((pokemon, i) => {
-            return <Card key={i} pokemon={pokemon} />;
-          })}
-        </div>
-      )}
-    </div>
+    <>
+      <Navbar></Navbar>
+      <div className="App">
+        {loading ? (
+          <h1>ロード中・・・</h1>
+        ) : (
+          <div className="pokemonCardContainer">
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon} />;
+            })}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
