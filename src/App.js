091a6c52c6e304a1da607fd801b9145057d385b6
ryuji-i <fantaneo.orange@gmail.com>
2024-05-04 09:33:28 +0900

    done chapter no.11

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { getAllPokemon } from "./utils/pokemon";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       let res = await getAllPokemon(initialURL);
+      setLoading(false);
       console.log(res);
     };
     fetchPokemonData();
   }, []);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {loading ? (
+        <h1>ロード中・・・</h1>
+      ) : (
+        <h1>ポケモンデータを取得しました</h1>
+      )}
+    </div>
+  );
 }
 
 export default App;
